0011bf70707dcd2d8e4dce82a710aa717e5e7a29
SWaruppp <155567332+SWaruppp@users.noreply.github.com>
2024-11-26 14:47:58 +0530

    complete

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,10 @@
+import aboutImg from "../assets/images/about_img.png";
+import { FaExternalLinkAlt } from "react-icons/fa";
+
+const About = () => {
+  return (
+   <></>
+  );
+};
+
+export default About;
